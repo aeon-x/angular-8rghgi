4981945ce36cba5343ba397d089f96ff2554b611
aeon-x <talexcao@gmail.com>
2022-10-31 17:08:48 -0700

    3 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,17 @@
+import { HttpClient } from '@angular/common/http'; 
 import { Product } from './products';
 import { Injectable } from '@angular/core';
+
 /* . . . */
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = [];
-/* . . . */
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -21,4 +26,8 @@ export class CartService {
     return this.items;
   }
 /* . . . */
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 }
